45b506832b174a37e57bf01986fc5fe0b21eda95
Ibraim Denislyam <ibraimdenis123@gmail.com>
2024-03-22 13:26:36 +0100

    Move sai services request sender to utils package for reuse

--- utils/common.go
+++ utils/common.go
@@ -2,6 +2,10 @@ package utils
 
 import (
 	"encoding/json"
+	"fmt"
+	jsoniter "github.com/json-iterator/go"
+	"io"
+	"net/http"
 	"reflect"
 
 	"github.com/saiset-co/saiEthIndexer/config"
@@ -32,4 +36,51 @@ func RemoveContract(slice []config.Contract, s int) []config.Contract {
 
 func RemoveAddress(slice []string, s int) []string {
 	return append(slice[:s], slice[s+1:]...)
+}
+
+func SaiQuerySender(body io.Reader, address, token string) ([]byte, error) {
+	const failedResponseStatus = "NOK"
+
+	type responseWrapper struct {
+		Status string              `json:"Status"`
+		Error  string              `json:"Error"`
+		Result jsoniter.RawMessage `json:"result"`
+		Count  int                 `json:"count"`
+	}
+
+	req, err := http.NewRequest(http.MethodPost, address, body)
+	if err != nil {
+		return nil, err
+	}
+
+	req.Header.Set("Content-Type", "application/json")
+	req.Header.Set("Token", token)
+
+	res, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return nil, err
+	}
+
+	defer res.Body.Close()
+
+	resBytes, err := io.ReadAll(res.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	if res.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("%s", resBytes)
+	}
+
+	result := responseWrapper{}
+	err = jsoniter.Unmarshal(resBytes, &result)
+	if err != nil {
+		return nil, err
+	}
+
+	if result.Status == failedResponseStatus {
+		return nil, fmt.Errorf(result.Error)
+	}
+
+	return resBytes, nil
 }

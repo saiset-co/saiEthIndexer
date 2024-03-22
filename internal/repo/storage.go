74c8330a01cedc312e310f9c5516990a19c620a3
Ibraim Denislyam <ibraimdenis123@gmail.com>
2024-03-22 13:27:04 +0100

    Move sai services request sender to utils package for reuse

--- internal/repo/storage.go
+++ internal/repo/storage.go
@@ -2,12 +2,9 @@ package repository
 
 import (
 	"bytes"
-	"fmt"
-	"io"
-	"net/http"
-
 	jsoniter "github.com/json-iterator/go"
 	"github.com/saiset-co/sai-storage-mongo/external/adapter"
+	"github.com/saiset-co/saiEthIndexer/utils"
 )
 
 type Repo interface {
@@ -46,54 +43,7 @@ func (mr *MongoRepo) Create(data interface{}) error {
 		return err
 	}
 
-	_, err = mr.querySender(bytes.NewReader(payload))
+	_, err = utils.SaiQuerySender(bytes.NewReader(payload), mr.address, mr.token)
 
 	return err
-}
-
-func (mr *MongoRepo) querySender(body io.Reader) ([]byte, error) {
-	const failedResponseStatus = "NOK"
-
-	type responseWrapper struct {
-		Status string              `json:"Status"`
-		Error  string              `json:"Error"`
-		Result jsoniter.RawMessage `json:"result"`
-		Count  int                 `json:"count"`
-	}
-
-	req, err := http.NewRequest(http.MethodPost, mr.address, body)
-	if err != nil {
-		return nil, err
-	}
-
-	req.Header.Set("Content-Type", "application/json")
-	req.Header.Set("Token", mr.token)
-
-	res, err := http.DefaultClient.Do(req)
-	if err != nil {
-		return nil, err
-	}
-
-	defer res.Body.Close()
-
-	resBytes, err := io.ReadAll(res.Body)
-	if err != nil {
-		return nil, err
-	}
-
-	if res.StatusCode != http.StatusOK {
-		return nil, fmt.Errorf("%s", resBytes)
-	}
-
-	result := responseWrapper{}
-	err = jsoniter.Unmarshal(resBytes, &result)
-	if err != nil {
-		return nil, err
-	}
-
-	if result.Status == failedResponseStatus {
-		return nil, fmt.Errorf(result.Error)
-	}
-
-	return resBytes, nil
 }
